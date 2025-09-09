f5ca02616de7ed93cff7fa5181c31a7baa635f3a
Nguyen Thi Hai Duong <11235568@st.neu.edu.vn>
2025-09-09 16:41:20 +0700

    Công việc của Dương

--- ProjectThu3/haiduong.Designer.cs
+++ ProjectThu3/haiduong.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjectThu3
 {
-    partial class Form1
+    partial class haiduong
     {
         /// <summary>
         /// Required designer variable.
@@ -29,26 +29,37 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(291, 70);
+            this.label1.Location = new System.Drawing.Point(302, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(135, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Công việc của Dương";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Công việc của Dương";
+            // 
+            // haiduong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "haiduong";
+            this.Text = "haiduong";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
-
--- ProjectThu3/haiduong.cs
+++ ProjectThu3/haiduong.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ProjectThu3
 {
-    public partial class Form1 : Form
+    public partial class haiduong : Form
     {
-        public Form1()
+        public haiduong()
         {
             InitializeComponent();
         }

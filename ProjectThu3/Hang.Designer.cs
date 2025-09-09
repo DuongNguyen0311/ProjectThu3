98e3403ce699291e195d84dc4c2e89eac66d98da
thuhang011105 <ctthuhang10a1@gmail.com>
2025-09-09 16:49:05 +0700

    hello

--- ProjectThu3/Hang.Designer.cs
+++ ProjectThu3/Hang.Designer.cs
@@ -36,9 +36,9 @@
             this.Hello.AutoSize = true;
             this.Hello.Location = new System.Drawing.Point(148, 95);
             this.Hello.Name = "Hello";
-            this.Hello.Size = new System.Drawing.Size(126, 16);
+            this.Hello.Size = new System.Drawing.Size(205, 16);
             this.Hello.TabIndex = 0;
-            this.Hello.Text = "Hello anh em , tui nè";
+            this.Hello.Text = "Hello anh em , tui nè ăn cơm chưa";
             this.Hello.Click += new System.EventHandler(this.label1_Click);
             // 
             // label1

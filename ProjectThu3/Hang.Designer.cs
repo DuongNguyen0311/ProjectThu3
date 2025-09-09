5771c5771837f073a0e1f7236608872de7544f71
thuhang011105 <ctthuhang10a1@gmail.com>
2025-09-09 16:41:20 +0700

    Công việc Hằng đảm nhận

---
+++ ProjectThu3/Hang.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ProjectThu3
+{
+    partial class label1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Hello = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Hello
+            // 
+            this.Hello.AutoSize = true;
+            this.Hello.Location = new System.Drawing.Point(148, 95);
+            this.Hello.Name = "Hello";
+            this.Hello.Size = new System.Drawing.Size(39, 16);
+            this.Hello.TabIndex = 0;
+            this.Hello.Text = "Hello";
+            this.Hello.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Hello);
+            this.Name = "label1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Hello;
+    }
+}
